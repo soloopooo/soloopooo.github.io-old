1b279da8f2613e6ac58a05d0cff3b9b6eff912b6
soloopooo <36123081+soloopooo@users.noreply.github.com>
2019-02-14 02:10:39 +0000

    Create time.js

---
+++ assets/js/time.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    var loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart; 
+    console.log('Page load time is '+ loadTime);
+}
